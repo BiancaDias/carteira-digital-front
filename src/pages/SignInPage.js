c883cdbc60f278e21eae2a09e6c762d10049be8a
Bianca Dias <bianca.dias@ufv.br>
2023-04-18 12:42:29 -0300

    estruturação pagina de login

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -5,12 +5,34 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const url = "";
+  function login(e){
+    e.preventDefault();
+  }
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={login}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <input
+          placeholder="E-mail" 
+          type="email"
+          id="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
+        <input 
+          placeholder="Senha" 
+          type="password" 
+          //autocomplete="new-password" 
+          id="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
         <button>Entrar</button>
       </form>
 
